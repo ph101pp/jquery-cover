64fa56c924379b3cdfccc1108bc9618733fa8e6b
Philipp C. Adrian <philippcadrian@greenish.ch>
2013-07-21 23:18:26 +0200

    Rename jQuery greenishBackground -> jQuery Cover + adding read me

---
+++ jquery.cover.js
@@ -0,0 +1,166 @@
+/*! 
+ * jQuery Cover plugin - v1.0.0
+ * 
+ * Copyright (c) 2011 Philipp Adrian (www.philippadrian.com)
+ *
+ * The MIT Licence (http://opensource.org/licenses/MIT)
+ *   
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions: 
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+(function($) {
+////////////////////////////////////////////////////////////////////////////////
+var cover=$.fn.cover = function (method){	
+	if(typeof(method) === 'object' || !method) {
+		args=arguments;
+		call="_init";
+	}
+	else if(cover[method]) {
+		args=Array.prototype.slice.call(arguments,1);
+		call=method;
+	}
+	else throw "Error: The method \""+method+"\" doesn't exist in jQuery Cover";
+	
+	return $(this).each(function(){
+//		Make sure to select the image (and not a wrapper)
+		var img=$(this).is("img") ? $(this) : $("img", this),
+			data = img.data("data");
+			
+//			IF already initialized -> update options.
+			if(call == "_init" && data) {
+				cover._opts(data, method, true);
+				return;				
+			}
+			data = data || {
+				self:$(this),
+				callbacks : []
+			};
+			
+		if(call=="_init") {
+			data.opts=method=="_init"?Array.prototype.slice.call(arguments,1):method;
+			args=[data];
+		}
+		else args=[data].concat(args);
+		img.data("data", data);
+		if(call=="_triggerCallback") return cover[call].apply(img, args);
+		else try { 
+				cover[call].apply(img, args);
+			}
+			catch(err){
+				if(err!="callbackReturnedFalse") throw err;
+			}
+	});
+};
+$.extend(cover, {
+	interval:false,
+////////////////////////////////////////////////////////////////////////////////
+	_init : function (data) {
+		
+//		Extends defaults into opts.
+		var opts=cover._opts(data, data.opts, true),
+			css={},
+			wrapper;
+		data.img = new Image();
+
+//		binding callbacks to make them available.
+		for(callbacks in opts.callbacks) cover.bindCallback(data,callbacks,opts.callbacks[callbacks]);
+
+		data.img.src = data.self.attr("src");
+		data.self.wrap($("<div class=\"greenishCover\"\><div\>"));
+		data.wrapper=data.self.parent().parent();
+		
+		if(opts.backgroundPosition) {
+			if(opts.backgroundPosition.search(/top/i) >= 0) data.wrapper.addClass("top");
+			else if(opts.backgroundPosition.search(/bottom/i) >= 0) data.wrapper.addClass("bottom");
+			if(opts.backgroundPosition.search(/left/i) >= 0) data.wrapper.addClass("left");
+			else if(opts.backgroundPosition.search(/right/i) >= 0) data.wrapper.addClass("right");
+		}
+
+		data.self.cover("_triggerCallback","preLoading"); // #CALLBACK
+		if(!cover.interval) cover.interval=setInterval(cover._loading, 100);
+
+		data.opts.checkWindowResize && $(window)
+			.unbind("resize.cover")
+			.bind("resize.cover", function(){$(".greenishCover").cover("update")});
+	},
+////////////////////////////////////////////////////////////////////////////////
+	update : function() {
+		var that = $(this),
+			img=that.is("img") ? that : that.find("img"),
+			data = img.data("data"),
+			wrapper = data.wrapper;
+			current = wrapper.hasClass("height") ? "height":"width",
+			newRatio = wrapper.height()/wrapper.width() >= data.ratio ? "height":"width";
+
+		if(current !== newRatio) {
+			wrapper.addClass(newRatio).removeClass(current);
+			img.cover("_triggerCallback","ratioSwitch"); // #CALLBACK
+		}
+	},
+////////////////////////////////////////////////////////////////////////////////
+	_loading : function () {
+		cover.complete=true;
+		$(".greenishCover").each(function (){
+			var data = $("img", this).data("data"),
+				image= data.img;
+			if(!image || !image.complete || image.height <= 0 || image.width <= 0) {
+				cover.complete=false;
+				return;
+			}
+			data.ratio=image.height/image.width;
+			marginTop=50/image.width*image.height; // Based on the fact that (marginTop 100% == width).
+			data.self.css({"marginTop":-marginTop+"%"});
+			data.self.cover("_triggerCallback","postLoading"); // #CALLBACK
+			data.wrapper.cover("update");
+		});
+		if(cover.complete) {
+			clearInterval(cover.interval);
+			cover.interval=false;
+		}
+	},
+////////////////////////////////////////////////////////////////////////////////
+	bindCallback : function (data, callback, func) {
+		func=typeof(func)=="function"?[func]:func;
+		data.callbacks[callback]=data.callbacks[callback]||[];
+		for(var key in func) data.callbacks[callback].push(func[key]);
+	},
+////////////////////////////////////////////////////////////////////////////////
+	_triggerCallback : function (data, callback, param) {
+		if(!data.callbacks[callback] || data.callbacks[callback].length <= 0) return param;
+		for(var key in data.callbacks[callback]) 
+			if((param=data.callbacks[callback][key].apply(this, [data,param])) !== false) continue;
+			else throw "callbackReturnedFalse";
+		return param;
+	},	
+////////////////////////////////////////////////////////////////////////////////	
+	_opts : function (data, opts, save) {
+		opts=$.extend(true,{},this.defaults, data.opts||{}, opts||{});
+		if(save) data.opts=opts;
+		return opts;
+	},
+////////////////////////////////////////////////////////////////////////////////	
+	defaults : {
+		backgroundPosition:"center",
+		callbacks: {},
+		checkWindowResize:true
+	}
+});
+})(jQuery);
+
+
---
+++ readme.markdown
@@ -0,0 +1,176 @@
+#jQuery Cover plugin
+
+This plugin mimics the CSS3 `background-size:cover;` behavior and therefore makes it available in all Browsers. 
+
+
+#Documentation
+
+1.	General
+----------------
+	
+To start off, get the newest version of the jQuery Cover plugin from Github and move it into your project folder.
+
+Include the file into your HTML and you're ready to go:
+
+``` javascript
+jQuery(function(){
+	$("img.myBackgroundImage").cover();
+});
+```
+
+So the code above takes an image:
+
+``` html
+<body>
+	<img src="myBackground.jpg" class="myBackgroundImage">
+</body>
+```
+
+and makes it cover its parent element completly. 
+
+The new markup will look like this:
+
+``` html
+<body>
+	<div class="greenishCover width|height">
+		<div>
+			<img src="myBackground.jpg" class="myBackgroundImage">
+		</div>
+	</div>
+</body>
+```
+	
+1.	Methods
+----------------
+	
+``` javascript
+$(".myBackgroundImage").cover([Object options]);				
+```
+
+> ####Initialize / Update Options
+>
+> Initialize the jQuery Cover plugin on a set of Images.
+>
+> Optional you can pass an object with options (see below) to the function.
+>
+> Also you can change options with this at any time after initialisation.
+
+-------------------------
+	
+``` javascript
+$(".myBackgroundImage").cover("update");				
+```
+
+> #### Update
+>
+> Tests the aspect ratio of the container against the aspect ratio of the image and updates the display of the image if necessary.
+
+-------------------------
+	
+``` javascript
+$(".myBackgroundImage").slides("bindCallback", Function);				
+```
+
+> #### Bind a Callback/Event
+>
+> Registers or binds a callback function to a certain event. (see below). 
+
+
+1.	Options
+----------------
+
+Options can be set or changed by passing an object to the cover() function. This can be done on initialisation or afterwards to change any value.
+
+Here are all the possibile options (set to their __default values__):
+
+``` javascript
+$(".myBackgroundImage").cover({
+		backgroundPosition:"center",
+		checkWindowResize:true,
+		callbacks: {}
+	});
+```
+
+###Option Details
+
+``` javascript
+backgroundPosition:				String									Default: "center"
+```
+
+> Works similar to the CSS property ``background-position``. Can be any of the following: __"center"__, __"top"__, __"right"__, __"bottom"__, __"left"__ or > a combination of two like: __"top left"__ or __"bottom left"__.
+
+-------------------------	
+
+``` javascript
+checkWindowResize:				boolean									Default: true
+```
+
+> If true the plugin will automatically call ``$(".myBackgroundImage").cover("update");`` on window resize.
+
+-------------------------	
+
+``` javascript
+callbacks: {																Default: {}
+	"callbackName":				Function(callback function)
+								Object	(multiple callback functions)
+								Array	(multiple callback functions)	
+}	
+```
+
+
+> Allows to register callback functions during initialisation. 
+>
+> To learn more about the available callbacks look at the Callbacks/Events chapter below.
+
+-------------------------
+
+1.	Callbacks/Events
+----------------
+
+During the runtime of the plugin a bunch of callbacks are fired. 
+
+Callbacks can be registered by adding the functions to the options object or by calling the "bindCallback" method.
+
+Every callback function receives the _data_ object as first parameter while the following parameters can change.
+
+By returning __false__ in the callback the pluging cancels the current event and stopps its action. 
+
+This is a list of all the callbacks that are available at the moment (to find them in the code search for _#CALLBACK_):
+
+``` javascript
+"preLoading"					function(data)					Context: $(".myBackgroundImage")
+```
+
+> Called on initialisation before the image is loaded.
+
+-------------------------
+
+``` javascript
+"postLoading"					function(data)					Context: $(".myBackgroundImage")
+```
+
+> Called on initialisation after the image is loaded.
+
+-------------------------
+
+``` javascript
+"ratioSwitch"					function(data)					Context: $(".myBackgroundImage")
+```
+
+> Called when the display type of the image changes.
+
+
+###Add New Callback
+
+If you need a new callback that is missing, feel free to add it in the code!
+Just put the following at the place you need the call:
+
+``` javascript
+[context].cover("_triggerCallback","myCallbackName" [,parameterOne, parameterTwo, ...]);
+```
+
+Notice that the new callback will come with all the features of the default ones:
+* The _data_ object will always be passed as the first parameter. (And you can add as many additional parameters as you wish.)
+* return __false__ will stop the further execution of the acutal event.
+
+__When you added a new callback, tell me about it! So I can consider adding it to the default callbacks.__
